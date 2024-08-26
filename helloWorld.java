be0c44dfb4fc1a24c36e7da4577f17a249ee83e3
harshyadav1604 <yadav.pradeep@iitg.ac.in>
2024-08-26 22:36:51 +0530

    Sample Commit

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class helloWorld{
+    public static void main(String[] args) {
+
+        System.out.println("Hello World and Welcome to GitHub Trial Folder!");
+        
+    }
+
+}
