6d168821f4d1c644d1bd14addcd62df1d5ce4223
harshyadav1604 <yadav.pradeep@iitg.ac.in>
2024-08-27 09:56:39 +0530

    First Commit

---
+++ dateinJava.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;    
+
+public class dateinJava {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+    
+}
